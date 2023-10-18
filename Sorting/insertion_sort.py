4d6dffc09ddce333ef52ae3d830c87f8673a3d18
Mansi_Gupta <mansigg1106@gmail.com>
2023-10-18 20:48:18 +0530

    insertion_sort.py

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,10 +1,12 @@
 def insertion_sort(arr):
+    # Start iterating through the array from the second element (index 1)
     for i in range(1,len(arr)):
-        key=arr[i]
+        key=arr[i] # Select the current element as the key to be inserted
         j=i-1
+        # Move elements of arr[0..i-1] that are greater than key to one position ahead
         while(j>=0 and key< arr[j]):
             arr[j+1]=arr[j]
-            j-=1
+            j-=1 # Move to the previous element
         arr[j+1]=key
                 
     
