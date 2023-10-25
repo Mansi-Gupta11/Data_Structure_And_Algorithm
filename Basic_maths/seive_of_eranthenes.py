24afb964d026c1572d30497951141ab19e1d2e21
Mansi_Gupta <mansigg1106@gmail.com>
2023-10-25 10:24:06 +0530

    seive_of_eranthenes.py

--- Basic_maths/seive_of_eranthenes.py
+++ Basic_maths/seive_of_eranthenes.py
@@ -1,8 +1,9 @@
 n=10
+#n=int(input()) -- For taking users input
 res=[0]*(n+1)
 i=2
-res[0]=1
-res[1]=1
+res[0]=1  # Prime starts from 2 so in 0th and 1st index we will insert 1 so it doesnt count these both as prime.
+res[1]=1  
 ans=[]
 while(i*i<=n):
     if res[i]==0:
